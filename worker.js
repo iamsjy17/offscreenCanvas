91c4562c82931a86dc21e6c504e6b3fbd8baba2f
jewooSong <iamsjy0808@gmail.com>
2018-12-30 06:01:35 +0900

    오류 수정

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 self.importScripts("./drawtest.js");
 
 self.onmessage = event => {
-  const { canvas, width, height } = event.data.canvas;
+  const { canvas, width, height } = event.data;
   const context = canvas.getContext("2d");
 
   draw(context);
